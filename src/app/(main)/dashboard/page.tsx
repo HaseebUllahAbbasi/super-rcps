c0dfc61870a9ff06a0dfdd6093a05709b04b5ce1
sulemanahmedrajput <sulemanahmed2704@gmail.com>
2025-03-29 14:48:30 +0500

    added the color

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -3,7 +3,7 @@ const Dashboard = () => {
     <div className="flex min-h-screen items-center justify-center w-full  px-4 py-12 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div className="text-center">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+          <h1 className="text-3xl font-bold tracking-tight text-primary">Dashboard</h1>
           <p className="mt-2 text-sm text-gray-600">Welcome to your dashboard</p>
         </div>
       </div>
